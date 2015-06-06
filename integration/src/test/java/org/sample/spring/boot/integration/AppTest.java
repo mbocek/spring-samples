8a95095e662d97dd535283dca2fe01a379432de8
Michal Bocek <michal.bocek@gmail.com>
2015-06-06 22:15:49 +0200

    simple integration project

---
+++ integration/src/test/java/org/sample/spring/boot/integration/AppTest.java
@@ -0,0 +1,19 @@
+package org.sample.spring.boot.integration;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+	/**
+	 * Rigourous Test :-)
+	 */
+	@Test
+	public void testApp() {
+		assertTrue(true);
+	}
+}
